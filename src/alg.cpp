b3b481f4f7998dd82d68b82ee69b6afb9d9604e1
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-03-01 16:34:33 +0300

    Andrianova IST-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int y = 0, i = 0, j = size - 1; 
+    int mid = i + (j - i) / 2;
+    if (true) {
+        for (int i = 0; i < mid; i++) {
+            if (arr[i] == value)
+                y++;
+        }
+        for (int j = size; j >= mid; j--) {
+            if (arr[i] == value)
+                y++;
+        }
+        return y;
+    } else
+        return 0;
 }
+
