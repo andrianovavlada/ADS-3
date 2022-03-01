c64b46bcefca603eec1619e592dd7dbb6eef7b21
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-03-01 17:02:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 int cbinsearch(int *arr, int size, int value) {
     int y = 0, i = 0, j = size - 1;
     int mid = i + (j - i) / 2;
-   if (true) {
+    if (true) {
       for (int i = 0; i < mid; i++) {
             if (arr[i] == value)
                 y++;
